c87d7ce4675f0e83db277dedbdf8e6cd2d79a4ac
masteroffluff <chris.chapman@linuxmail.org>
2023-07-06 19:38:48 +0100

    fist commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let array = ["Lion", "Elephant", "Giraffe", "Tiger", "Cheetah", "Zebra", "Gorilla", "Kangaroo", "Koala", "Dolphin", "Whale", "Shark", "Penguin", "Polar bear", "Grizzly bear", "Wolf", "Fox", "Owl", "Eagle", "Hawk", "Parrot", "Toucan", "Snake", "Crocodile", "Alligator", "Turtle", "Frog", "Butterfly", "Bee", "Ant", "Spider", "Octopus", "Jellyfish", "Crab", "Lobster", "Seal", "Penguin", "Otter", "Squirrel", "Rabbit", "Deer", "Moose", "Horse", "Cow", "Pig", "Sheep", "Goat", "Chicken", "Duck", "Fish"]
+
+
+
+console.log(`embed in a string  => ${array[0]}`);
+
